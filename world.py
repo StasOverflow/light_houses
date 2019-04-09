e5579a66841ccd8f0c5ec7a7e7e3f8e2996ac878
Vladimir Striukov <vladimir.alexeevich.morrowind@gmail.com>
2019-04-09 19:54:58 +0300

    project reinit

---
+++ world.py
@@ -0,0 +1,32 @@
+class HouseHold:
+
+    def __init__(self):
+        self.neighbour_list = list()
+        self.power_plant_list = list()
+
+
+class World:
+
+    def create_power_plant(self):
+        raise NotImplementedError("To be implemented")
+
+    def create_household(self):
+        raise NotImplementedError("To be implemented")
+
+    def connect_household_to_power_plant(self, household, power_plant):
+        raise NotImplementedError("To be implemented")
+
+    def connect_household_to_household(self, household1, household2):
+        raise NotImplementedError("To be implemented")
+
+    def disconnect_household_from_power_plant(self, household, power_plant):
+        raise NotImplementedError("To be implemented")
+
+    def kill_power_plant(self, power_plant):
+        raise NotImplementedError("To be implemented")
+
+    def repair_power_plant(self, power_plant):
+        raise NotImplementedError("To be implemented")
+
+    def house_hold_has_electricity(self, household):
+        raise NotImplementedError("To be implemented")
